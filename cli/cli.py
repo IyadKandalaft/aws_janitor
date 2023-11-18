caf2e8c84a47c3546cbca85ff96dc672ef593ed1
Iyad Kandalaft <ik@iyadk.com>
2023-11-17 21:26:28 -0500

    Added global debug flag to the CLI

--- cli/cli.py
+++ cli/cli.py
@@ -1,24 +1,33 @@
 import logging
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, 
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 import argparse
 import importlib
 
 from .plugin_manager import load_plugins, initialize_plugins
 
+def setup_logging(debug=False):
+    level = logging.DEBUG if debug else logging.INFO
+    logging.basicConfig(level=level, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 def main():
+    # Create a parser for global arguments
+    global_parser = argparse.ArgumentParser(description="AWS Janitor CLI", add_help=False)
+    global_parser.add_argument('-d', '--debug', action='store_true', help='Enable debug mode')
+
+    global_args, remaining_argv = global_parser.parse_known_args()
+    setup_logging(global_args.debug)
+
     logger.debug("Initializing main parser")
     parser = argparse.ArgumentParser(description="AWS Janitor CLI")
+    parser.add_argument('-d', '--debug', action='store_true', help='Enable debug mode')
     subparsers = parser.add_subparsers(dest="resource", help="Resource types")
 
     load_plugins()
     initialize_plugins(subparsers)
 
-    args = parser.parse_args()
-    # Handle the arguments and invoke the appropriate plugin logic
+    args = parser.parse_args(remaining_argv)
 
 if __name__ == "__main__":
     main()
